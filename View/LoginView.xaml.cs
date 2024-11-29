10b8a0b54c52041084b6b458e6e996b8a93a6a9e
KoshkinVitaliy <kosh98vitaliy@outlook.com>
2024-11-29 11:30:30 +0300

    Добавьте файлы проекта.

---
+++ View/LoginView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesignApp.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Page
+    {
+        private Window mainWindow;
+        public LoginView(Window mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                mainWindow.DragMove();
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignApp
+{
+    public class Worker
+    {
+        private Int64 id;
+        private string surname;
+        private string name;
+        private string lastname;
+        private string job_title;
+        private string login;
+        private string password;
+    }
+}
